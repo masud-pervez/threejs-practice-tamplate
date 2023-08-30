846e1add3f20b4ce7d1eb817e95d7bfcb68d2e1e
masud-pervez <masudpervez431@gmail.com>
2023-08-30 23:13:53 +0600

    customize something

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 const App = () => {
-  return <div>Project Setup</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
